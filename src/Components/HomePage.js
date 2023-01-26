37029a05038f11cbe7cee74ebcd491088db8f07a
Hitesh Pawar <hiteshpawar9298@gmail.com>
2023-01-26 10:19:00 +0530

    Integrated Home Page Assignment 1

---
+++ src/Components/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = ({ name }) => {
+  return (
+    <center>
+      <h1 style={{ color: "purple" }}>Welcome, {name} !!!</h1>
+    </center>
+  );
+};
+
+export default HomePage;
